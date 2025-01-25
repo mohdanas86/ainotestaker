429bbfe0641bc05a4d97ff9488391c9d32d6c7cc
mohdanas86 <anasalam7739@gmail.com>
2025-01-25 09:36:27 +0530

    next config header http index prob

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,78 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      // Disallow indexing for specific paths
+      {
+        source: "/",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index, follow", // Ensure the home page is indexed and links are followed
+          },
+        ],
+      },
+      {
+        source: "/about-us",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index, follow", // Ensure the home page is indexed and links are followed
+          },
+        ],
+      },
+      {
+        source: "/sign-in",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex, nofollow",
+          },
+        ],
+      },
+      {
+        source: "/sign-up",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex, nofollow",
+          },
+        ],
+      },
+      {
+        source: "/dashboard",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex, nofollow",
+          },
+        ],
+      },
+      {
+        source: "/dashboard/upgrade",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex, nofollow",
+          },
+        ],
+      },
+      {
+        source: "/workspace",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex, nofollow",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
